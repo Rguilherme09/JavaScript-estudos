d57edd7b98b5d94c728e0e71aa83a9d99394295c
Rodrigo Guilherme <rguilherme419@hotmail.com>
2024-12-23 16:42:43 -0300

    Exercícios de aprendizado

---
+++ readline-sync/conversor.js
@@ -0,0 +1,9 @@
+import entradaDados from 'readline-sync';
+
+console.log("Conversor de Milhas para Quilômetros: \n");
+
+let milhas = entradaDados.question("Informe o valor em Milhas: ");
+
+let km = milhas / 0.62137;
+
+console.log(`${milhas} milhas equivale a ${km.toFixed(2)}Km`);
